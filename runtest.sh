4624aeedd41c6acdaada10458dde9aafb6013cff
Yu Feng <yfeng1@berkeley.edu>
2016-01-14 18:12:46 -0800

    test tar-pip.sh.

--- runtest.sh
+++ runtest.sh
@@ -3,8 +3,9 @@ export OMP_NUM_THREADS=1
 
 source ./activate.sh /tmp/local "mpirun -n 2"
 
+bash tar-pip.sh six.tar.gz six
 # send the anaconda packages
-bcast -v anaconda.tar.gz 
+bcast -v anaconda.tar.gz  six.tar.gz
 
 # location of MPI4PY in /dev/shm/local
 
