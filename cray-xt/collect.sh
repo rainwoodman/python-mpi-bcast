2dfc03d9b7f77cfd4272e2c5c91223ff1fbbd57a
Yu Feng <rainwoodman@gmail.com>
2016-01-15 00:01:46 -0600

    Add benchmarks on cray-xt BlueWaters

---
+++ cray-xt/collect.sh
@@ -0,0 +1,18 @@
+cat >process.awk <<EOF
+NR == 49 {printf("%g ", \$3);}
+NR == 50 {printf("%g ", \$3);}
+NR == 51 {printf("%g ", \$3);}
+NR == 54 {
+       split(\$2, a, "m"); 
+       printf("%g ", a[1] * 60 + a[2]);
+}
+NR == 61 {
+      split(\$2, a, "m"); 
+      printf("%g \n", a[1] * 60 + a[2]);
+}
+EOF
+echo "# N bcast tar bcasttot0 bcasttot launch"
+for i in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096; do
+    echo -n "$i "
+    awk -f process.awk bench-$i.out
+done
--- cray-xt/run-bench.sh
+++ cray-xt/run-bench.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 sed -e "s;@N@;$1;g" bench.template > bench-$1.job
-qsub -q normal bench-$1.job
+qsub -q low bench-$1.job
