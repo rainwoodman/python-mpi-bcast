914c139ce6a0785f91996158fde80aacbefd715f
Yu Feng <yfeng1@berkeley.edu>
2017-08-10 18:03:10 -0700

    set PYTHONHASHSEED.
    see https://github.com/rainwoodman/pfft-python/issues/5

--- activate.sh
+++ activate.sh
@@ -30,6 +30,7 @@ if [[ -n $_PYTHONMPIBCASTBCASTROOT ]]; then
     export PYTHONPATH=$_PYTHONMPIBCASTBCASTROOT/lib/python
     export PYTHONHOME=$_PYTHONMPIBCASTBCASTROOT
     export PYTHONUSERBASE=$_PYTHONMPIBCASTBCASTROOT
+    export PYTHONHASHSEED=0 # important to make the non-cyclic object/dict deconstruction collective
     export LD_LIBRARY_PATH=$_PYTHONMPIBCASTBCASTROOT/lib:$LD_LIBRARY_PATH
     export PATH=$_PYTHONMPIBCASTBCASTROOT/bin:$PATH
 
